667699cd66547eb137b6e0139d487c1e39e560da
Pasho Kevin <kevin.pasho7@etu.univ-lorraine.fr>
2026-03-31 15:21:34 +0200

    ajout squelette telecommande

---
+++ projett/src/Telecommande.java
@@ -0,0 +1,26 @@
+public class Telecommande {
+    private Lampe []lampes;
+    public Telecommande(){
+    }
+
+    public void ajouterLampe(Lampe l){
+        throw new Error (" code non ecrit ");
+    }
+
+    public void activerLampe(int indiceLampe){
+        throw new Error (" code non ecrit ");
+    }
+
+    public void activerTout(){
+        throw new Error (" code non ecrit ");
+    }
+
+    public String toString(){
+        throw new Error (" code non ecrit ");
+    }
+
+    public Lampe getLampe(int indiceLampe){
+        throw new Error (" code non ecrit ");
+    }
+
+}
